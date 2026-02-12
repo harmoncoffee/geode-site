dbd0bd9ff93e5ba82a4d708f01f2881a3c700845
Bryan Behrenshausen <bryan.behrenshausen@sas.com>
2026-02-12 17:42:25 -0500

    Update button styling and text justification

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -173,7 +173,8 @@ function TopAboutSection() {
                     <p className={styles.aboutTagline}>
                     Apache Geode™ is a data management platform that provides real-time, consistent access to data-intensive applications throughout widely distributed cloud architectures.
                     </p>
-                        <a className={clsx('button button--primary button--lg', styles.downloadButton)} href="https://github.com/apache/geode/releases">Download Geode</a>
+						<a className={clsx('button button--lg', styles.primaryButton)} href="https://github.com/apache/geode/releases">Download Geode</a>
+						<a className={clsx('button button--lg', styles.secondaryButton)} href="https://github.com/apache/geode/releases">Learn more</a>
                 </div>
             </div>
         </div>
